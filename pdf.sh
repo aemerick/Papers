41e7871088f5c1a9102a1363bb38b8eb837d1951
Andrew Emerick <aemerick11@gmail.com>
2019-03-07 15:47:13 -0500

    commit

---
+++ pdf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+convert DD0136_fiducial_shortrad_nort.png DD0136_fiducial_shortrad_nort.pdf
+convert DD0160_fiducial_shortrad_nort.png DD0160_fiducial_shortrad_nort.pdf
+convert mass_density_cut.png mass_density_cut.pdf
+convert mass_loading_comparison.png mass_loading_comparison.pdf
+convert mass_outflow_comparison.png mass_outflow_comparison.pdf
+convert mass.png mass.pdf
+convert metal_retention.png metal_retention.pdf
+convert sfr.png sfr.pdf
